6f6dee10d4ecbb531a5a71dc5b466226aa5b5c25
Sora <dart238@email.com>
2024-01-22 10:27:38 -0500

    Template

---
+++ routes/auth.js
@@ -0,0 +1,13 @@
+import { Router } from 'express'
+import * as authCtrl from '../controllers/auth.js'
+
+const router = Router()
+
+/*---------- Public Routes ----------*/
+router.post('/signup', authCtrl.signup)
+router.post('/login', authCtrl.login)
+
+/*---------- Protected Routes ----------*/
+
+
+export { router }
---
+++ server.js
@@ -0,0 +1,36 @@
+// import npm packages
+import 'dotenv/config.js'
+import express from 'express'
+import logger from 'morgan'
+import cors from 'cors'
+
+// connect to MongoDB with mongoose
+import './config/database.js'
+
+// import routes
+import { router as profilesRouter } from './routes/profiles.js'
+import { router as authRouter } from './routes/auth.js'
+
+// create the express app
+const app = express()
+
+// basic middleware
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+
+// mount imported routes
+app.use('/api/profiles', profilesRouter)
+app.use('/api/auth', authRouter)
+
+// handle 404 errors
+app.use(function (req, res, next) {
+  res.status(404).json({ err: 'Not found' })
+})
+
+// handle all other errors
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500).json({ err: err.message })
+})
+
+export { app }
