6194f4fb7d35493ea5fd6fa81275efbcccd42cb2
fritzhuie <fritzhuie@gmail.com>
2024-01-23 11:23:53 -0500

    controllers/quiz.js

---
+++ controllers/quiz.js
@@ -0,0 +1,89 @@
+
+
+async function getQuizHistory(req, res) {
+  try {
+    // get current user
+
+    // for each in User.profile.quizzes (quizId) {
+      // Quiz.getMany( quizId ) 
+
+      return "getQuizHistory called"
+  } catch (error) {
+
+  }
+}
+
+async function getQuizDetails(req, res) {
+  try {
+    // get current user
+
+    // for each in User.profile.quizzes (quizId) {
+      // Quiz.getMany( quizId ) 
+
+      return "getQuizDetails called"
+  } catch (error) {
+
+  }
+}
+
+async function getQuiz(req, res) {
+  try {
+    // get current user
+
+    // for each in User.profile.quizzes (quizId) {
+      // Quiz.getMany( quizId ) 
+
+      return "getQuiz called"
+  } catch (error) {
+
+  }
+}
+
+async function answerQuiz(req, res) {
+  try {
+    // get current user
+
+    // for each in User.profile.quizzes (quizId) {
+      // Quiz.getMany( quizId ) 
+
+      return "answerQuiz called"
+  } catch (error) {
+
+  }
+}
+
+async function removeQuizFromHistory(req, res) {
+  try {
+    // get current user
+
+    // for each in User.profile.quizzes (quizId) {
+      // Quiz.getMany( quizId ) 
+
+      return "removeQuizFromHistory called"
+  } catch (error) {
+
+  }
+}
+
+async function provideFeedback(req, res) {
+  try {
+    // get current user
+
+    // for each in User.profile.quizzes (quizId) {
+      // Quiz.getMany( quizId ) 
+
+      return "provideFeedback called"
+  } catch (error) {
+
+  }
+}
+
+
+export {
+  getQuizHistory,
+  getQuizDetails,
+  getQuiz,
+  answerQuiz,
+  removeQuizFromHistory,
+  provideFeedback
+}
