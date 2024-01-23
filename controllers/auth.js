f6a600b5fc5ee23a1c3607bc26373726493e705f
Ade <https://github.com/AdeShennaike>
2024-01-23 10:54:19 -0500

    create models

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 
 import { User } from '../models/user.js'
-import { Profile } from '../models/profile.js'
+import { Profile } from '../models/quiz.js'
 
 /*----- Helper Functions -----*/
 
