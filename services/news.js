bf0bb8d345f5339e418f9981e7cd5261b1736b2f
fritzhuie <fritzhuie@gmail.com>
2024-01-25 21:27:03 -0500

    add news service

---
+++ services/news.js
@@ -0,0 +1,72 @@
+const news = [
+  {
+    category: "",
+    author: "",
+    source: "",
+    headline: "",
+    date: "",
+    content: ["", "", ""]
+  },
+  {
+    category: "Business / Tech",
+    author: "Brian Fung, CNN",
+    source: "CNN",
+    headline: "{jp:アップルがヨーロッパのアプリに関する大規模な変更を発表|en:Apple announces sweeping changes for apps in Europe}, including allowing third-party app stores for the first time",
+    date: "Updated: 4:29 PM EST, Thu January 25, 2024",
+    content: [
+      "Apple announced sweeping new changes to its handling of {jp:アプリケーション|en:apps} in {jp:ヨーロッパ|en:Europe} on {jp:木曜日|en:Thursday}, including plans to allow third-party {jp:アプリストア|en:app stores} on {jp:iPhonesとiPads|en:iPhones and iPads} for the first time in {jp:会社|en:company} history and significant cuts to Apple’s {jp:アプリストア|en:app store} fees.",
+      "The unprecedented {jp:更新|en:updates}, which are a response to new {jp:ヨーロッパの規制|en:European regulations} set to take effect in {jp:3月|en:March}, reflect some of the most consequential changes to Apple’s {jp:アプリ|en:app} business since the debut of its proprietary {jp:アプリストア|en:app store} 15 years ago, which forms the bedrock of its {jp:壁のある庭|en:walled garden} ecosystem.",
+      "For {jp:消費者|en:consumers}, the shift may mean more {jp:選択肢|en:choices} in how they acquire and install {jp:アプリ|en:apps}. And for {jp:開発者|en:developers}, other changes to Apple’s {jp:規約|en:terms} may give them more {jp:柔軟性|en:flexibility} in how they market themselves to {jp:ユーザー|en:users}.",
+      "The moves underscore how {jp:欧州連合|en:European Union} policymakers have successfully forced Apple to change its {jp:ビジネス慣行|en:business practices} in the face of vocal {jp:苦情|en:complaints} from developers who have accused the {jp:iPhone|en:iPhone} maker of anticompetitive or monopolistic {jp:行動|en:behavior}.",
+      "Under the changes, Apple said it will allow {jp:ユーザー|en:users} to download third-party {jp:アプリストア|en:app stores} onto their {jp:デバイス|en:devices} from {jp:ウェブサイト|en:websites} outside of Apple’s own {jp:エコシステム|en:ecosystem}. The {jp:アプリストア|en:app stores}, Apple said, will exist as standalone {jp:アプリ|en:apps} on an {jp:iOSデバイス|en:iOS device} with the authority to install other {jp:アプリ|en:apps} offered by those third-party {jp:マーケットプレイス|en:marketplaces}.",
+      "Apple also said it would offer to slash the {jp:手数料|en:fees} it collects from in-app transactions involving digital {jp:商品とサービス|en:goods and services}, reducing them from 30% to 17% and, for developers that are eligible for certain {jp:割引プログラム|en:discount programs} through Apple, from 15% to 10%."
+    ]
+  },
+  {
+    category: "{ar:أعمال / إعلام|en:Business / Media}",
+    author: "{ar:أوليفر دارسي، سي إن إن|en:by Oliver Darcy, CNN}",
+    source: "CNN",
+    headline: "{ar:عودة جون ستيوارت إلى 'ذا ديلي شو'|en:Jon Stewart’s return to ‘The Daily Show’} could shake up 2024 politics",
+    date: "{ar:محدث: 7:08 صباحًا بتوقيت شرق الولايات المتحدة، الخميس 25 يناير 2024|en:Updated: 7:08 AM EST, Thu January 25, 2024}",
+    content: [
+        "{ar:جون ستيوارت يعود إلى منزله القديم في 'ذا ديلي شو'|en:Jon Stewart is returning to his old home at 'The Daily Show'} — {ar:لكن الحي الذي تقع فيه الدار الكوميدية قد تغير بشكل كبير منذ أن توقف عن تقديم البرنامج|en:but the neighborhood in which the comedic house resides has dramatically changed since he last signed off from host chair}.",
+        "{ar:ستيوارت، الذي قدم البرنامج الشهير لمدة 16 عامًا، غادر كوميدي سنترال في عام 2015|en:Stewart, who hosted the iconic program for 16 years, decamped Comedy Central in 2015} — {ar:تزامنًا مع صعود دونالد ترامب إلى السلطة وتمزق أوصال السياسة الأمريكية|en:just as Donald Trump ascended to power and as the seams of American politics tore apart}. {ar:بينما لا ينقص المواد الخام الساخرة القادمة من واشنطن لكوميديين لاستخدامها كنكات، يعود ستيوارت إلى تشكيلة الكابل في بيئة متضررة من عاصفة المعلومات السيئة المزاج|en:While there is never a shortage of material pouring out of Washington for comedians to use as punchlines, Stewart returns to the cable lineup in an environment battered by an ill-tempered information monsoon}.",
+        "{ar:المعلومات المضللة ونظريات المؤامرة والأكاذيب الصريحة تشبع الخطاب العام لعام 2024 بطريقة تتناقض بشكل صارخ مع العصر السابق لترامب الذي كان ستيوارت يعمل فيه سابقًا|en:Misinformation, conspiracy theories, and outright lies saturate the 2024 public discourse in a way that stands in stark contrast to the pre-Trump era in which Stewart previously operated}. {ar:ترامب وحلفاؤه السياسيون من MAGA لديهم في تصرفهم آلة إعلامية قوية تضخ الدعاية في الحوار الوطني وترسلها فيروسية على أساس يومي|en:Trump and his stable of MAGA political allies have at their disposal a powerful media machine that pumps propaganda into the national conversation and sends it viral on a daily basis}.",
+        "{ar:عندما كان ستيوارت يدير 'ذا ديلي شو'|en:Back when Stewart helmed 'The Daily Show'}, {ar:كان يستفز أشخاصًا أكثر استقرارًا مثل شخصية الأخبار المحافظة ب"
+    ]
+  },
+  {
+    category: "{ar:السعودية تغير قوانين الكحول|en:Saudi Arabia to change alcohol rules}",
+    author: "{ar:مصطفى سالم، سي إن إن|en:By Mostafa Salem, CNN}",
+    source: "CNN",
+    date: "{ar:محدث: 9:31 صباحًا بتوقيت شرق الولايات المتحدة، الخميس 25 يناير 2024|en:Updated: 9:31 AM EST, Thu January 25, 2024}",
+    content: [
+        "{ar:تم الكشف عن قواعد جديدة للكحول في المملكة العربية السعودية|en:New rules on alcohol in Saudi Arabia} have been revealed ahead of the reported opening of the first-ever {ar:متجر لبيع الخمور|en:liquor store} in the capital {ar:الرياض|en:Riyadh}.",
+        "The rules regulating {ar:حصص الكحول|en:alcohol quotas} for non-Muslim diplomatic missions are being introduced to {ar:مواجهة التجارة غير المشروعة للبضائع الكحولية|en:counter the illicit trade of alcohol goods},” the governmental {ar:مركز الاتصالات الدولية|en:Center of International Communication} (CIC) said in a statement.",
+        "{ar:ذكرت رويترز مصدرًا مطلعًا على الخطط ووثيقة، قائلة إن المملكة العربية السعودية تستعد لفتح أول متجر لبيع الكحول في العاصمة الرياض|en:Reuters cited a source familiar with the plans, and a document, saying that Saudi Arabia is preparing to open its first alcohol store in the capital Riyadh} which will serve exclusively non-Muslim diplomats.",
+        "In response to the {ar:تقرير رويترز|en:Reuters report}, the CIC told CNN that a {ar:عملية جديدة ستركز على تخصيص كميات محددة من بضائع الكحول عند دخول المملكة|en:new process will focus on allocating specific quantities of alcohol goods when entering the Kingdom} to put an end to the previous unregulated process that caused an uncontrolled exchange of such goods in the Kingdom.",
+        "The CIC did not clarify if a shop will be opened but said the new {ar:الإطار التنظيمي تم تقديمه لمواجهة التجارة غير المشروعة للبضائع الكحولية والمنتجات التي تتلقاها البعثات الدبلوماسية|en:regulatory framework has been introduced to counter the illicit trade of alcohol goods and products received by diplomatic missions},” the CIC said.",
+        "{ar:بيع واستهلاك الكحول في المملكة العربية السعودية محظور بشكل صارم|en:Alcohol sale and consumption in Saudi Arabia is strictly forbidden}, and is punishable by lashes, deportations, fines and imprisonment.",
+        "However, the nation under {ar:الأمير محمد بن سلمان|en:Crown Prince Mohamed bin Salman} has moved towards the remodeling of its {ar:صناعة السياحة|en:tourism industry} as part of a grand vision to diversify the economy. That has led to speculation that it could {ar:تقنين الكحول في المملكة|en:legalize alcohol} in the Kingdom in the near future."
+    ]
+  },
+  {
+    category: "{zh:世界 / 亚洲|en:World / Asia}",
+    author: "{zh:布拉德·伦登，CNN|en:By Brad Lendon, CNN}",
+    source: "CNN",
+    date: "{zh:更新时间：2024年1月25日上午7:48东部标准时间|en:Updated: 7:48 AM EST, Thu January 25, 2024}",
+    content: [
+        "“{zh:巨大的|en:Gigantic}” {zh:海浪|en:waves} swamped parts of a key {zh:美国军事设施|en:US military facility} in the middle of the {zh:太平洋|en:Pacific Ocean} last weekend, causing damage that will take months to repair, according to a {zh:美国军方|en:US Army} report.",
+        "A video posted on X showed the terrifying surge of {zh:水|en:water} rushing into a dining facility on {zh:罗伊-纳姆尔岛|en:Roi-Namur island}, the second-largest island of {zh:夸贾林环礁|en:Kwajalein Atoll}, which hosts a {zh:美国军事弹道导弹防御测试站点|en:US military ballistic missile defense test site} in the {zh:马绍尔群岛共和国|en:Republic of the Marshall Islands}.",
+        "The rushing {zh:水|en:water} broke down doors and windows and pushed furniture around the facility as it reached near ceiling height in the video.",
+        "“{zh:快离开这里|en:Get out of here},” one person could be heard yelling just before the power cuts out and darkness envelops the facility at around 9 p.m. Saturday.",
+        "In a video posted on Facebook, {zh:科尔·德鲁·摩根上校，美国陆军夸贾林环礁驻军指挥官|en:Col. Drew Morgan, commander of US Army Garrison-Kwajalein Atoll (USAG-KT)}, said there were only minor injuries from the “unpredicted, gigantic {zh:海浪|en:waves}” that washed over the north point of the tiny island.",
+        "But the {zh:美国陆军|en:US Army} report said damage to the island’s infrastructure was extensive.",
+        "“{zh:岛上多个区域被水淹没|en:Multiple areas on the island are under water},” according to the Army statement, which was posted and accompanied by an aerial photo taken on January 21 that shows extensive flooding on {zh:罗伊-纳姆尔岛|en:Roi-Namur}.",
+        "The {zh:跑道|en:runway} on the island, home to 120 personnel, needed to be cleared so recovery operations could begin, according to the statement from {zh:美国陆军夸贾林环礁驻军|en:USAG-KT}.",
+        "The island’s housing, automotive complex, theater and chapel were damaged, it said.",
+        "“{zh:罗伊恢复行动|en:Operation Roi Recovery} could take months to complete,” the Army said.",
+        "The report did not mention any damage to {zh:军事基础设施|en:military infrastructure}."
+    ]
+  }
+]
